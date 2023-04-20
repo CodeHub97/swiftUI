7139b6c272187a80c9c587efcd9ef4ab28dec250
Gaganpreet <Gaganpreet@offsureit.com>
2023-04-20 09:40:46 +0530

    Initial Commit

---
+++ SwiftUI100Days/SwiftUI100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100DaysApp.swift
+//  SwiftUI100Days
+//
+//  Created by Offsureit Solutions on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
